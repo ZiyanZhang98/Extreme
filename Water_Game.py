9d36fb6a639aa55439a4a74b25db74e4290bee8d
Ziyan Zhang <44527704+Ziyan-Zhang1129@users.noreply.github.com>
2019-09-28 17:48:37 +0100

    Alcohol Time ~

--- Water_Game.py
+++ Water_Game.py
@@ -1,3 +1,10 @@
+def find_min_card(contestant, i):
+    alist = list(contestant[i])
+    card = min(alist)
+    # 这个肯定是不对的，不过我饿了，晚上再想
+    return card
+
+
 line1 = input()
 n = int(line1[0])
 k = int(line1[2])
@@ -5,5 +12,6 @@ contestant = []
 for i in range(n):
     contestant.append(input())
 # contestant[k-1] += 'w'
+i = 0
 while contestant[i] != contestant[i][::-1]:
-    raner 
+        turn = 1

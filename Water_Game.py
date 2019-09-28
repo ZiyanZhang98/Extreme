4bf53d216089a06acf6dfe5af21304f47d4bfc71
Ziyan Zhang <44527704+Ziyan-Zhang1129@users.noreply.github.com>
2019-09-28 16:36:03 +0100

    something something

---
+++ Water_Game.py
@@ -0,0 +1,9 @@
+line1 = input()
+n = int(line1[0])
+k = int(line1[2])
+contestant = []
+for i in range(n):
+    contestant.append(input())
+# contestant[k-1] += 'w'
+while contestant[i] != contestant[i][::-1]:
+    raner 
